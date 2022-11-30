c254c93b971e6c3c6fb8834e6c57950d25c1b843
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-11-30 15:22:38 +0400

    feat: add routes for login page and reset password pages

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\DashboardPageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,9 +17,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::view('/', 'dashboard.index')->name('dashboard');
+Route::get('/countries', [DashboardPageController::class, 'showStatistics'])->name('dashboard.countries');
+
 //registration
 Route::view('/register', 'session.register')->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
 Route::get('/user/verify/{token}', [RegisterController::class, 'verifyEmail'])->name('user.verify');
 
+//login
 Route::view('/login', 'session.login')->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+//reset password
+Route::get('forget-password', [ForgotPasswordController::class, 'showForgetPasswordForm'])->name('forget.password.get');
+Route::post('forget-password', [ForgotPasswordController::class, 'submitForgetPasswordForm'])->name('forget.password.post');
+Route::get('reset-password/{token}', [ForgotPasswordController::class, 'showResetPasswordForm'])->name('reset.password.get');
+Route::post('reset-password', [ForgotPasswordController::class, 'submitResetPasswordForm'])->name('reset.password.post');
