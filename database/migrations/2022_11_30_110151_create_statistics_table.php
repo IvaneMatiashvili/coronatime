b51c1c6333115fcb52364ca2e22f7397f1cd51de
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-11-30 17:00:23 +0400

    fix: create a statistics schema

--- database/migrations/2022_11_30_110151_create_statistics_table.php
+++ database/migrations/2022_11_30_110151_create_statistics_table.php
@@ -4,28 +4,32 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('statistics', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+return new class extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('statistics', function (Blueprint $table) {
+			$table->id();
+			$table->string('country');
+			$table->unsignedBigInteger('confirmed');
+			$table->unsignedBigInteger('recovered');
+			$table->unsignedBigInteger('critical');
+			$table->unsignedBigInteger('deaths');
+			$table->timestamps();
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('statistics');
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('statistics');
+	}
 };
