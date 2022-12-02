4dad22b774f3715d44a1e481bff2c4edd6e3028c
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-12-02 12:31:07 +0400

    feat: create a function which shows country statistics

--- app/Http/Controllers/DashboardPageController.php
+++ app/Http/Controllers/DashboardPageController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Statistic;
 
 class DashboardPageController extends Controller
 {
-    //
+	public function index()
+	{
+		return view(
+			'dashboard.index',
+			[
+				'user'                 => auth()->user(),
+				'worldwide'            => Statistic::latest('created_at')->first(),
+			]
+		);
+	}
+
+	public function showCountriesStatistics()
+	{
+		return view('dashboard.country-statistics', [
+			'statistics'           => Statistic::filter(request()->query())->get(),
+			'user'                 => auth()->user(),
+			'worldwide'            => Statistic::latest('created_at')->first(),
+		]);
+	}
 }
