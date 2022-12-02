379a93b4220b098853bca7077f9428384ec3d12f
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-12-02 17:17:52 +0400

    fix: remove worldwide database

--- app/Http/Controllers/DashboardPageController.php
+++ app/Http/Controllers/DashboardPageController.php
@@ -8,21 +8,35 @@ class DashboardPageController extends Controller
 {
 	public function index()
 	{
+		$worldwide = [
+			'name'                     => 'Worldwide',
+			'confirmed'                => Statistic::sum('confirmed'),
+			'recovered'                => Statistic::sum('recovered'),
+			'critical'                 => Statistic::sum('critical'),
+			'deaths'                   => Statistic::sum('deaths'),
+		];
 		return view(
 			'dashboard.index',
 			[
 				'user'                 => auth()->user(),
-				'worldwide'            => Statistic::latest('created_at')->first(),
+				'worldwide'            => $worldwide,
 			]
 		);
 	}
 
 	public function showCountriesStatistics()
 	{
+		$worldwide = [
+			'name'                     => 'Worldwide',
+			'confirmed'                => Statistic::sum('confirmed'),
+			'recovered'                => Statistic::sum('recovered'),
+			'critical'                 => Statistic::sum('critical'),
+			'deaths'                   => Statistic::sum('deaths'),
+		];
 		return view('dashboard.country-statistics', [
 			'statistics'           => Statistic::filter(request()->query())->get(),
 			'user'                 => auth()->user(),
-			'worldwide'            => Statistic::latest('created_at')->first(),
+			'worldwide'            => $worldwide,
 		]);
 	}
 }
