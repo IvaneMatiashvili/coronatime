0748c6941eaac6224f0d0970dafc46fd8a8d3dcd
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-12-02 12:30:03 +0400

    feat: create sorting and filter for statistics data

--- app/Models/Statistic.php
+++ app/Models/Statistic.php
@@ -10,4 +10,41 @@ class Statistic extends Model
 	use HasFactory;
 
 	protected $guarded = [];
+
+	public function scopeFilter($query, array $filters)
+	{
+		$query->when(
+			$filters['search'] ?? false,
+			fn ($query, $search) => $query
+				->where('country', 'LIKE', "%{$search}%")
+				->where('country', '!=', 'Worldwide')
+		);
+		foreach ($filters as $kay => $param)
+		{
+			if ($kay === 'search')
+			{
+				continue;
+			}
+			$query->when(
+				$param ?? false,
+				function ($query, $param) use ($kay) {
+					if ($param === 'asc')
+					{
+						$query->
+						orderBy($kay, 'asc');
+					}
+					else
+					{
+						$query->
+						orderBy($kay, 'desc');
+					}
+				}
+			);
+		}
+		$query->when(
+			!$filters,
+			fn ($query) => $query
+				->where('country', '!=', 'Worldwide')
+		);
+	}
 }
