434df37512ae9977bc492afd22529c16c7d73e44
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-12-02 12:24:42 +0400

    feat: add a name in dashboard header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,8 +1,9 @@
+@props(['username'])
 <header class="bg-yellow w-full h-[5rem] flex justify-between items-center">
     <x-svg.coronatime class="ml-[9.25rem]"/>
     <div class="flex justify-between items-center mr-[6.75rem]">
         <p class="mr-4 font-bold text-[1rem]">
-            name
+            {{ $username }}
         </p>
         <span class="border border-dark-20 h-[2rem] mr-4"></span>
         <form method="post" action="{{ route('logout') }}">
