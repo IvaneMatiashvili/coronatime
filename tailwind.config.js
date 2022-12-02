5d102435f6f6233d31ccf9092183b2e398dfd8e6
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-12-02 12:22:00 +0400

    feat: add background images and new colors in tailwind css config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,21 @@ module.exports = {
         colors: {
               'dark-100': '#010414',
               'dark-60': '#808189',
+              'dark-40': '#BFC0C4',
               'dark-20': '#E6E6E7',
               'dark-4': '#F6F6F7',
               'blue-border': '#2029F3',
               'green-button': '#0FBA68',
               'green-box': '#249E2C',
+              'green-recovered': '#0FBA68',
+              'blue-cases': '#2029F3',
+              'yellow-death': '#EAD621',
           },
+        backgroundImage: {
+            'blue-bg': "url('/public/images/new-cases.png')",
+            'green-bg': "url('/public/images/recovered.png')",
+            'yellow-bg': "url('/public/images/death.png')",
+      }
     },
   },
   plugins: [
