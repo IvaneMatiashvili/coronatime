a20d9a8d255333b148ef5d04b93c531a2164e2b7
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-12-02 12:25:48 +0400

    feat: create a show statistics header

---
+++ resources/views/components/statistics-header.blade.php
@@ -0,0 +1,57 @@
+@props(['name','title', 'route_asc', 'route_desc'])
+
+
+<x-flex.row class="h-full w-[6rem] justify-evenly">
+    <p class="text-[0.875rem] font-semibold text-dark-100 w-[6rem]">
+        {{ $title }}
+    </p>
+    @if(request()->query($name) && request()->query($name) === 'asc')
+
+        <x-flex.col class="justify-start h-[0.6rem]">
+
+            <a href="{{ $route_asc }}" class="w-0 h-0
+                          border-l-[5px] border-l-transparent
+                          border-b-[5px] border-b-dark-100
+                          border-r-[5px] border-r-transparent cursor-pointer">
+            </a>
+
+            <a href="{{ $route_desc }}" class="w-0 h-0
+                            border-l-[5px] border-l-transparent
+                            border-t-[5px] border-t-dark-40
+                            border-r-[5px] border-r-transparent mt-[3px] cursor-pointer">
+            </a>
+        </x-flex.col>
+    @elseif(request()->query($name) &&  request()->query($name) === 'desc')
+        <x-flex.col class="justify-between h-[0.6rem]">
+
+            <a href="{{ $route_asc }}" class="w-0 h-0
+                          border-l-[5px] border-l-transparent
+                          border-b-[5px] border-b-dark-40
+                          border-r-[5px] border-r-transparent cursor-pointer">
+            </a>
+
+            <a href="{{ $route_desc }}" class="w-0 h-0
+                            border-l-[5px] border-l-transparent
+                            border-t-[5px] border-t-dark-100
+                            border-r-[5px] border-r-transparent mt-[3px] cursor-pointer">
+            </a>
+        </x-flex.col>
+
+    @else
+        <x-flex.col class="justify-between h-[0.6rem]">
+
+            <a href="{{ $route_asc }}" class="w-0 h-0
+                          border-l-[5px] border-l-transparent
+                          border-b-[5px] border-b-dark-40
+                          border-r-[5px] border-r-transparent cursor-pointer">
+            </a>
+
+            <a href="{{ $route_desc }}" class="w-0 h-0
+                            border-l-[5px] border-l-transparent
+                            border-t-[5px] border-t-dark-40
+                            border-r-[5px] border-r-transparent mt-[3px] cursor-pointer">
+            </a>
+        </x-flex.col>
+
+    @endif
+</x-flex.row>
